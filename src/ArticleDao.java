df0cf9139df7e739e36058fa498e8a6db16aae11
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-21 15:34:06 +0200

    Création ArticleDao

---
+++ src/ArticleDao.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class ArticleDao {
+
+    public void create(Article article){
+
+    }
+
+    public Article findById(int id){
+
+    }
+
+    public List<Article> findAll(){
+
+    }
+
+    public void update(Article article){
+
+    }
+
+    public void delete(int id){
+
+    }
+}
