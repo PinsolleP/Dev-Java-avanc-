851b99bde152746ffa79ca4dc70c138c7aa03651
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-21 14:55:32 +0200

    création du dossier src et class article

---
+++ src/Article.java
@@ -0,0 +1,61 @@
+public class Article {
+    private int identifiant;
+    private String description;
+    private String brand;
+    private float price;
+
+    public Article(int identifiant, String description, String brand, float price) {
+        this.identifiant = identifiant;
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public Article(String description, String brand, float price){
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString(){
+        return "Article{" +
+                "identifiant=" + identifiant +
+                ", description ='" + description + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
