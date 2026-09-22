12bcee7deaa7ad65d1191f0fbfbb8f6d3f2e3e8e
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-22 08:30:59 +0200

    Test connexion bdd

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,14 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 
 public class Main {
 
     public static void main(String[] args){
 
-        String url = "jdbc:mariadb://localhost:3306/shop";
-        String user = "root";
-        String password = "Pinsolle40!";
-
         try {
-            Connection connection = DriverManager.getConnection(
-                    url,
-                    user,
-                    password
-            );
-            String sql = "SELECT * FROM t_articles";
+            Connection connection = Database.getConnection();
 
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()){
-                int id = resultSet.getInt("IdArticle");
-                String description = resultSet.getString("Description");
-                String brand = resultSet.getString("Brand");
-                float price = resultSet.getFloat("UnitaryPrice");
-
-                Article article = new Article (
-                        id,
-                        description,
-                        brand,
-                        price
-                );
-
-                System.out.println(article);
-            }
+            System.out.println("Connexion réussie !");
 
             connection.close();
 
