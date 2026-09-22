c5e1e569f0a6aa39628d58f8e4d98c2ca8470786
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-22 08:30:22 +0200

    Ajout méthode findbyid

--- src/ArticleDao.java
+++ src/ArticleDao.java
@@ -1,13 +1,53 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ArticleDao {
 
-    public void create(Article article){
+    public void create(Article article) throws SQLException {
+        String sql = """
+                INSERT INTO t_articles(Description, Brand, UnitaryPrice)
+                VALUES (?, ?, ?)
+                """;
 
+        try (Connection connection = Database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1, article.getDescription());
+            statement.setString(2, article.getBrand());
+            statement.setFloat(3, article.getPrice());
+
+            statement.executeUpdate();
+        }
     }
 
-    public Article findById(int id){
+    public Article findById(int id) throws SQLException {
+        String sql = """
+                SELECT IdArticle, Description, Brand, UnitaryPrice
+                FROM t_articles
+                WHERE IdArticle = ?
+                """;
 
+        try (Connection connection = Database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                if (resultSet.next()) {
+                    return new Article(
+                            resultSet.getInt("IdArticle"),
+                            resultSet.getString("Description"),
+                            resultSet.getString("Brand"),
+                            resultSet.getFloat("UnitaryPrice")
+                    );
+                }
+            }
+        }
+        return null;
     }
 
     public List<Article> findAll(){
